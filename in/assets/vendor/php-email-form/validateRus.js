385c971a70c6a47beff5831626957e22dc3a1f77
diyorbek-alikuziev <diyorbekpppp@gmail.com>
2023-06-07 14:18:00 +0500

    first commit

--- in/assets/vendor/php-email-form/validateRus.js
+++ in/assets/vendor/php-email-form/validateRus.js
@@ -64,7 +64,7 @@
         thisForm.querySelector('.sent-message').classList.add('d-block');
         thisForm.reset(); 
       } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
+        throw new Error(data ? data : 'Отправка формы не удалась, и сообщение об ошибке не возвращалось из: ' + action); 
       }
     })
     .catch((error) => {
